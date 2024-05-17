9a4585cd95b1ce42567f994c1050b2948062a58c
Kheil <cunanankheilanthony@gmail.com>
2024-05-17 14:50:30 +0800

    2nd sem update

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ function average(){
     let e = document.getElementById("e").value;
     let f = document.getElementById("f").value;
     let g = document.getElementById("g").value;
-    let h = document.getElementById("h").value;
-    let i = document.getElementById("i").value;
-    let total = (a*4)+(b*3)+(c*3)+(d*1)+(e*3)+(f*3)+(g*1)+(h*3)+(i*3);
-    let avg = total/24;
+    let total = (a*3)+(b*4)+(c*4)+(d*3)+(e*2)+(f*3)+(g*3);
+    let avg = total/22;
     document.getElementById("avg").textContent = parseFloat(avg.toFixed(4));
 }
